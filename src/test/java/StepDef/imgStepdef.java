458b2bdb5ffb9bb88f2d7017c767d3427f8a03cc
Rinsath07 <rinshad6263@gmail.com>
2024-01-06 10:20:53 +0530

    few chnages

--- src/test/java/StepDef/imgStepdef.java
+++ src/test/java/StepDef/imgStepdef.java
@@ -1,16 +1,21 @@
 package StepDef;
 
 import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.specification.ProxySpecification;
+import io.restassured.specification.RequestSpecification;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
 
 public class imgStepdef {
     @Given("upload img")
     public void uploadImg() {
-
+       // RestAssured.proxy= ProxySpecification.host("").withPort(0);
         File f=new File("Feature/Pet.feature");
       String r=  given()
                 .contentType(ContentType.MULTIPART)
@@ -21,5 +26,16 @@ public class imgStepdef {
                 .when()
                 .post("https://petstore.swagger.io/v2/pet/{id}/uploadImage").asPrettyString();
         System.out.println(r);
+        try(FileOutputStream fg=new FileOutputStream(""))
+        {
+
+        }
+        catch (Exception o)
+        {
+
+        }
+        finally {
+            
+        }
     }
 }
